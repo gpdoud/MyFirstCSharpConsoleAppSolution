12543b645b68706092c097283b04636fbd53871e
Greg Doud <gpdoud@gmail.com>
2021-07-02 14:06:49 -0400

    Add project files.

---
+++ MyFirstCSharpConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyFirstCSharpConsoleApp {
+    class Program {
+
+        static void Main(string[] args) {
+
+            for(var i = 1; i <= 30; i++) {
+                if(i % 3 == 0 && i % 5 == 0) {
+                    Console.Write("FizzBuzz ");
+                    continue;
+                }
+                if(i % 3 == 0) {
+                    Console.Write("Fizz ");
+                    continue;
+                }
+                if (i % 5 == 0) {
+                    Console.Write("Buzz ");
+                    continue;
+                }
+                Console.Write($"{i} ");
+            }
+
+        }
+
+    }
+}
